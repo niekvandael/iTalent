97b8f2ec81591eae5bd40332661a636bb81ee344
niekvandael <nvd@asist.be>
2016-04-20 17:28:21 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/java/be/italent/controllers/UserController.java
+++ italent/src/main/java/be/italent/controllers/UserController.java
@@ -1,11 +1,14 @@
 package be.italent.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
+	
+	@RequestMapping("/login")
 	public boolean login(){
-
 		return true;
 	}
+	
 }
