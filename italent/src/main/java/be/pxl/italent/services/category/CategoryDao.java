a3a44a672651a4ab7acbf7e225b6dcbd4325e9fc
niekvandael <nvd@asist.be>
2016-04-21 17:59:21 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

---
+++ italent/src/main/java/be/pxl/italent/services/category/CategoryDao.java
@@ -0,0 +1,9 @@
+package be.pxl.italent.services.category;
+
+import java.util.List;
+
+import be.italent.model.Category;
+
+public interface CategoryDao {
+	public List<Category> getCategories();
+}
---
+++ italent/src/main/java/be/pxl/italent/services/category/CategoryDaoMemImpl.java
@@ -0,0 +1,45 @@
+package be.pxl.italent.services.category;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Repository;
+
+import be.italent.model.Category;
+
+@Repository
+public class CategoryDaoMemImpl implements CategoryDao {
+	private Map<Integer, Category> categories;
+
+	@PostConstruct
+	public void init(){
+	}
+	
+	public synchronized List<Category> getCategories() {
+		//
+		// TODO: Read this from database
+		//
+		List<Category> result = new ArrayList<Category>();
+		
+		Category cat1 = new Category();
+		cat1.setId(1);
+		cat1.setDescription("Category nr 1");
+		cat1.setLastUpdate(Calendar.getInstance());
+		cat1.setLastUpdatedBy("NIEK");
+		cat1.setStatus("A");
+		
+		Category cat2 = new Category();
+		cat2.setId(1);
+		cat2.setDescription("Category nr 2");
+		cat2.setLastUpdate(Calendar.getInstance());
+		cat2.setLastUpdatedBy("NIEK");
+		cat2.setStatus("A");
+		
+		return result;
+	}
+
+}
