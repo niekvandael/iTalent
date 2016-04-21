8e5e1fdc5e0b67088d0d9a8ab68a911fffc41b41
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-21 18:17:53 +0200

    67 - Implement model ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=67 )

---
+++ italent/src/main/java/be/italent/model/Role.java
@@ -0,0 +1,70 @@
+package be.italent.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Role implements Serializable{
+	private static final long serialVersionUID = -6207223955990641498L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Size(max=20)
+	private String name;
+	
+	@OneToMany(mappedBy="role")
+	private List<User> users = new ArrayList<User>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
--- italent/src/main/java/be/italent/model/User.java
+++ italent/src/main/java/be/italent/model/User.java
@@ -1,20 +1,15 @@
 package be.italent.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@NamedQueries({
-	@NamedQuery(name="findAllActivatedUsernames", query="select u.username from User as u where u.confirmed=true"),
-	@NamedQuery(name="findAllUsers", query="select u from User as u")})
 public class User implements Serializable{
 	private static final long serialVersionUID = 2013292116325497059L;
 
@@ -22,34 +17,59 @@ public class User implements Serializable{
 	@GeneratedValue
 	private long id;
 	
+	@Size(min=2, max=55)
+	private String firstname;
+	
+	@Size(min=2, max=55)
+	private String lastname;
+	
+	@ManyToOne
+	private Role role;
+	
 	@NotNull
-	@Size(min=2, max=50)
+	@Size(min=2, max=55)
 	@Column(unique = true)
 	private String username;
 	
 	@NotNull
 	@Size(min=8, max=80)
 	private String password;	
-	
-	private String testing;
-	
-	private boolean confirmed;
-
 
 	public long getId() {
 		return id;
 	}
 
-
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 
 	public String getUsername() {
 		return username;
 	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
@@ -65,40 +85,18 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-
-	public boolean isConfirmed() {
-		return confirmed;
-	}
-
-
-	public void setConfirmed(boolean confirmed) {
-		this.confirmed = confirmed;
-	}
-
-
-	public String getTesting() {
-		return testing;
-	}
-
-
-	public void setTesting(String testing) {
-		this.testing = testing;
-	}
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (confirmed ? 1231 : 1237);
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result
-				+ ((password == null) ? 0 : password.hashCode());
-		result = prime * result
-				+ ((username == null) ? 0 : username.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -109,14 +107,27 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (confirmed != other.confirmed)
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
 			return false;
 		if (id != other.id)
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
 			return false;
 		if (password == null) {
 			if (other.password != null)
 				return false;
 		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
 			return false;
 		if (username == null) {
 			if (other.username != null)
@@ -125,7 +136,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
 }
