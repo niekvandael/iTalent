8a5161de032806212fc41aa21b28c6b004684548
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-21 14:07:55 +0200

    68 - Test Model ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=68 )

--- italent/src/main/java/be/italent/model/ITalentEntity.java
+++ italent/src/main/java/be/italent/model/ITalentEntity.java
@@ -1,9 +1,12 @@
 package be.italent.model;
 
 import javax.validation.constraints.Size;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE) //Least normalisation strategy  (jesse testing)
 public abstract class ITalentEntity {
 	protected char status;
 	@Size(max=55)
