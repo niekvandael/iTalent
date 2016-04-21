5bf295093d1dbef04b87f3f89f11836fec8ebadd
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-21 20:29:48 +0200

    69 - Implement Services ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=69 )

--- italent/src/main/java/be/italent/services/category/CategoryDaoMemImpl.java
+++ italent/src/main/java/be/italent/services/category/CategoryDaoMemImpl.java
@@ -17,7 +17,7 @@ public class CategoryDaoMemImpl implements CategoryDao {
 	
 	public synchronized List<Category> getCategories() {
 		
-		TypedQuery<Category> query = em.createQuery("select c from Category as m order by m.created desc", Category.class);
+		TypedQuery<Category> query = em.createQuery("select c from Category as c order by c.description desc", Category.class);
 	    return query.getResultList();
 		//
 		// TODO: Read this from database
