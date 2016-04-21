568ab197790cdd3160defc0027c760e88656379c
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-21 14:52:36 +0200

    68 - Test Model ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=68 )

--- italent/src/main/java/be/italent/model/Category.java
+++ italent/src/main/java/be/italent/model/Category.java
@@ -1,15 +1,11 @@
 package be.italent.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 @Entity
 public class Category extends ITalentEntity{
-	@Id
-	@GeneratedValue
-	private long id;
+	
 	
 	
 	@Size(max=200)
@@ -30,33 +26,5 @@ public class Category extends ITalentEntity{
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id != other.id)
-			return false;
-		return true;
 	}
 }
--- italent/src/main/java/be/italent/model/ITalentEntity.java
+++ italent/src/main/java/be/italent/model/ITalentEntity.java
@@ -1,18 +1,35 @@
 package be.italent.model;
 
 import javax.validation.constraints.Size;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 @Inheritance(strategy=InheritanceType.TABLE_PER_CLASS) //slightly more normalized 
 public abstract class ITalentEntity {
+	@Id
+	@GeneratedValue
+	protected long id;
+	
 	protected char status;
 	@Size(max=55)
 	protected String lastUpdatedBy;
 	@Temporal(TemporalType.TIMESTAMP)
-	protected String lastUpdateTimestamp;
+	protected Date lastUpdateTimestamp;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public char getStatus() {
 		return status;
 	}
@@ -25,10 +42,11 @@ public abstract class ITalentEntity {
 	public void setLastUpdatedBy(String lastUpdatedBy) {
 		this.lastUpdatedBy = lastUpdatedBy;
 	}
-	public String getLastUpdateTimestamp() {
+	public Date getLastUpdateTimestamp() {
 		return lastUpdateTimestamp;
 	}
-	public void setLastUpdateTimestamp(String lastUpdateTimestamp) {
+	public void setLastUpdateTimestamp(Date lastUpdateTimestamp) {
 		this.lastUpdateTimestamp = lastUpdateTimestamp;
 	}
+	
 }
