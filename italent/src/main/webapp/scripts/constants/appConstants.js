82f80bbbbccf43923d0585fab31d797e6aafb951
niekvandael <nvd@asist.be>
2016-04-23 14:13:10 +0200

    -  ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id= )

--- italent/src/main/webapp/scripts/constants/appConstants.js
+++ italent/src/main/webapp/scripts/constants/appConstants.js
@@ -6,6 +6,6 @@
 		.constant('GLOBALS', {
 			appName: '2016 - iTalent App',
 			appVersion: '1.0.0',
-			baseURL: location.origin.contains("rhcloud") ? location.origin + "/ws/" : location.origin + "/italent/ws/"
+			baseURL: location.origin.includes("rhcloud") ? location.origin + "/ws/" : location.origin + "/italent/ws/"
 		});
 })();
