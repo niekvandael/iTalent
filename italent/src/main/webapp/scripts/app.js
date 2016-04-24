d3276e199a411b0e41d1d172f5ca986515dc2afe
APSchuurman <a.p.schuurman@gmail.com>
2016-04-24 15:16:50 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/webapp/scripts/app.js
+++ italent/src/main/webapp/scripts/app.js
@@ -2,7 +2,7 @@
  * Created by arjen on 05/04/16.
  */
 (function () {
-    angular.module('iTalentApp', ['ngRoute', 'ngResource'])
+    angular.module('iTalentApp', ['ngRoute', 'ngResource', 'pascalprecht.translate', 'tmh.dynamicLocale', 'ngCookies'])
         .run(function ($rootScope) {
             $rootScope.user = {
                 base64: '',
@@ -34,5 +34,19 @@
                 .otherwise({
                     redirectTo: '/home'
                 })
+        })
+        .config(function ($translateProvider) {
+            $translateProvider.useStaticFilesLoader({
+                prefix: 'resources/locale-',// path to translations files
+                suffix: '.json'// suffix, currently- extension of the translations
+            });
+            $translateProvider.preferredLanguage('nl_NL');// is applied on first load
+            $translateProvider.useLocalStorage();// saves selected language to localStorage
+        })
+        .config(function ($translateProvider) {
+            $translateProvider.useMissingTranslationHandlerLog();
+        })
+        .config(function (tmhDynamicLocaleProvider) {
+            tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         })
 })();
