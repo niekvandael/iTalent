2c5e52ef3d319182a9ce9a7ac3f85ab22df00d39
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-25 23:45:47 +0200

    68 - Test Model ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=68 )

--- italent/src/test/java/be/italent/model/TestModel.java
+++ italent/src/test/java/be/italent/model/TestModel.java
@@ -4,18 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import be.italent.controllers.CategoryRestController;
 
 public class TestModel {
 
 	@Test
 	  public void testModel() {
-		CategoryRestController categoryRestController = new CategoryRestController();
+		//CategoryRestController categoryRestController = new CategoryRestController();
 		
 		Category categrory = new Category();
 		categrory.setDescription("HEALTH");
 		
-		categoryRestController.getCategories();
+		//categoryRestController.getCategories();
 		  
 	    // assert statements
 	    assertEquals(true, true);
