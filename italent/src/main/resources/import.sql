c4a505a233edda0a98d5287f9814997ae2688ad3
denniegrondelaers <dennie.grondelaers@gmail.com>
2016-04-27 21:49:33 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/resources/import.sql
+++ italent/src/main/resources/import.sql
@@ -1,31 +1,31 @@
 --
 -- Import script when "hibernate.hbm2ddl.auto" is set to "create"
 --
-delete from Department;
-delete from Category;
-delete from Role;
-delete from User;
-delete from Project;
-delete from Likes;
+delete from department;
+delete from category;
+delete from role;
+delete from user;
+delete from project;
+delete from likes;
 
 
 -- Departments
-insert into Department (id, lastUpdate, lastUpdatedBy, status, name) values(null, '2016-01-01', 'Team 1', 'A', 'IT');
-insert into Department (id, lastUpdate, lastUpdatedBy, status, name) values(null, '2016-01-01', 'Team 1', 'A', 'Marketing');
+insert into department (id, last_update, last_updated_by, status, name) values(null, '2016-01-01', 'Team 1', 'A', 'IT');
+insert into department (id, last_update, last_updated_by, status, name) values(null, '2016-01-01', 'Team 1', 'A', 'Marketing');
 
 -- Categories
-insert into Category (id, lastUpdate, lastUpdatedBy, status, description) values(null, '2016-01-01', 'Team 1', 'Active', 'Food');
+insert into category (id, last_update, last_updated_by, status, description) values(null, '2016-01-01', 'Team 1', 'Active', 'Food');
 
 -- Roles
-insert into Role (id, name) values(null, 'Student');
+insert into role (id, name) values(null, 'Student');
 
 -- Users
-insert into User (id, firstname, lastname, password, username, department_id, role_id) values(null, 'Team 1', 'Great', 'test', 'team1', 1, 1);
+insert into user (id, firstname, lastname, password, username, department_id, role_id) values(null, 'Team 1', 'Great', 'test', 'team1', 1, 1);
 
 -- Projects
-insert into Project (id, lastUpdate, lastUpdatedBy, status, beginDate, creationDate, description, endDate, isPublic, isVerified, projectStatus, shortDescription, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
-insert into Project (id, lastUpdate, lastUpdatedBy, status, beginDate, creationDate, description, endDate, isPublic, isVerified, projectStatus, shortDescription, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
-insert into Project (id, lastUpdate, lastUpdatedBy, status, beginDate, creationDate, description, endDate, isPublic, isVerified, projectStatus, shortDescription, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
+insert into project (id, last_update, last_updated_by, status, begin_date, creation_date, description, end_date, is_public, is_verified, project_status, short_description, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
+insert into project (id, last_update, last_updated_by, status, begin_date, creation_date, description, end_date, is_public, is_verified, project_status, short_description, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
+insert into project (id, last_update, last_updated_by, status, begin_date, creation_date, description, end_date, is_public, is_verified, project_status, short_description, title, category_id, user_id) values(null, '2016-04-23', 'Niek', 'A', '2016-05-01', '2016-04-23', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-06-01', false, false, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m', 1, 1);
 
 -- Likes
-insert into Likes(user_id, project_id, status, lastUpdatedBy, lastUpdate, id) values(1,1,'A','Niek', '2016-01-01', 1);
+insert into likes(user_id, project_id, status, last_update, last_updated_by, id) values(1,1,'A','Niek', '2016-01-01', 1);
