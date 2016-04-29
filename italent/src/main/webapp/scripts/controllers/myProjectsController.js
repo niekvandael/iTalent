a03c493b2a9c62c7e0358f67f4bd071304e2d4f5
APSchuurman <a.p.schuurman@gmail.com>
2016-04-29 22:38:26 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/webapp/scripts/controllers/myProjectsController.js
+++ italent/src/main/webapp/scripts/controllers/myProjectsController.js
@@ -2,8 +2,11 @@
  * Created by arjen on 05/04/16.
  */
 angular.module('iTalentApp')
-    .controller('myProjectsController', ['$scope', function ($scope) {
+    .controller('myProjectsController', ['$scope', '$location', function ($scope, $location) {
 
         $scope.myProjects = 'My projects page';
        
+        $scope.newProject = function() {
+            $location.path('/newProject');
+        };
     }]);
