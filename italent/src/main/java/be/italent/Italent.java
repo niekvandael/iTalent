b27eff471325c30ee6bc8b2f7b9432e0e144c0cf
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-30 05:26:59 +0200

    77 - Spring-boot setup and configuration ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=77 )

--- italent/src/main/java/be/italent/Italent.java
+++ italent/src/main/java/be/italent/Italent.java
@@ -1,5 +1,6 @@
 package be.italent;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
