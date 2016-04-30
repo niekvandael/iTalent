3efdf19cef4379637e187c009e475ca39a46f0a3
JesseVranken <jesse.vranken@student.pxl.be>
2016-04-30 06:54:02 +0200

    77 - Spring-boot setup and configuration ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=77 )

--- italent/src/main/java/be/italent/Italent.java
+++ italent/src/main/java/be/italent/Italent.java
@@ -1,4 +1,7 @@
 package be.italent;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +10,8 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class Italent extends SpringBootServletInitializer {
 
+	private static final Logger logger = LogManager.getLogger(Italent.class.getName());
+	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Italent.class);
@@ -14,6 +19,7 @@ public class Italent extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Italent.class, args);
+        logger.error("________ iTalent gestart ________");
     }
 }
 
