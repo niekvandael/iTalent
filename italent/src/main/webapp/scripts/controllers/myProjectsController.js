12305dff8f8af644c0b68fde6556ac62bbc86478
APSchuurman <a.p.schuurman@gmail.com>
2016-05-03 21:00:11 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/webapp/scripts/controllers/myProjectsController.js
+++ italent/src/main/webapp/scripts/controllers/myProjectsController.js
@@ -4,13 +4,35 @@
 angular.module('iTalentApp')
      .controller('myProjectsController', ['$scope', '$rootScope', '$location', 'projectService', 'likeService', function ($scope, $rootScope, $location, projectService, likeService) {
 
+    	$scope.myProjectsIsCollapsed = true;
+    	$scope.subscribedProjectsIsCollapsed = true;
+    	$scope.likedProjectsIsCollapsed = true;
+    	$scope.myProjects = {};
+    	 
     	projectService.listUser().then(function (projects) {
-            $scope.projects = projects;
+            $scope.myProjects = projects;
         }, function (err) {
             console.log('Error getting projects: ' + err)
         });
        
         $scope.newProject = function() {
             $location.path('/newProject');
+        };
+        
+        $scope.showDetails = function(id) {
+            $location.path('/projects/' + id);
+        };
+        
+        $scope.likeClicked = function (project) {
+            if (!project.numberOfLikes) {
+                project.numberOfLikes = 0;
+            }
+            if (project.liked) {
+                project.numberOfLikes--;
+            } else {
+                project.numberOfLikes++;
+            }
+            project.liked = !project.liked;
+            likeService.saveOrUpdate(project.id);
         };
     }]);
