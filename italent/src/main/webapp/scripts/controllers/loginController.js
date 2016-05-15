3491529b056054cb9330a1df42ca5b0c519ecc1a
APSchuurman <a.p.schuurman@gmail.com>
2016-05-15 17:15:30 +0200

    84 - Security setup ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=84 )

--- italent/src/main/webapp/scripts/controllers/loginController.js
+++ italent/src/main/webapp/scripts/controllers/loginController.js
@@ -3,19 +3,6 @@ angular.module('iTalentApp')
         // $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
         $scope.loginSubmit = function () {
-
-            /**
-             * 
-             * Opmerkingen:
-             * 
-             * Ik heb de 'authenticated' variabele van mijn oorspronkelijk project gebruikt.
-             * Waarschijnlijk is dit hetzelfde als Arjen's 'loggedIn' variabele en het mijne mag dan ook 
-             * weggeflikkerd worden zolang het op elke plaats vervangen wordt door 'loggedIn'.
-             * 
-             * Ik heb enkel dit bestand en login.html aangepast.
-             * Daar heb ik oa de 'ng-model'-naam veranderd en userName naar username gewijzigd.
-             * 
-             */
             
             /* Do authentication check */
             var authenticate = function(credentials, callback) {
@@ -32,7 +19,6 @@ angular.module('iTalentApp')
                         /* When success, do this */
                         $rootScope.authenticated = true;
                         $rootScope.user.displayName = $scope.credentials.username;
-                        $rootScope.loggedIn = true;
                         $location.path('/');
                     } else {
                         /* When failed, do this */
@@ -46,47 +32,13 @@ angular.module('iTalentApp')
             };
 
             authenticate();
-
-            //TODO Code die in mijn origineel security project stonden, geen idee of er nog iets bruikbaars tussenstaat.
-            /* self.credentials = {};
-            self.login = function() {
-                authenticate(self.credentials, function(authenticated) {
-                    if (authenticated) {
-                        console.log("Login succeeded");
-                        $location.href("#/");
-                        self.error = false;
-                        $rootScope.authenticated = true;
-                    } else {
-                        console.log("Login failed");
-                        $location.href("#/login");
-                        self.error = true;
-                        $rootScope.authenticated = false;
-                    }
-                })
-            }; */
-            
-            
-            //TODO Arjen's oorspronkelijke code
-            // var username = $scope.login.username;
-            // var docentOrNot = username.substring(username.length-7, username.length+1);
-            //
-            // $rootScope.user.displayName = username;
-            // $rootScope.loggedIn = true;
-            //
-            // if (docentOrNot == "@pxl.be")
-            // 	location.href = "#/docent";
-            // else
-            // 	location.href = "#/student";
-
-
         };
 
         $scope.logout = function () {
             $http.post('logout', {}).finally(function() {
                 $rootScope.user.displayName = '';
                 $rootScope.authenticated = false;
-                $rootScope.loggedIn = false;
-                $location.href("#/");
+                $location.path('/');
             });
         }
     }]);
