93fc852a906c3c338fe21ab09245aee7dd0c4d2e
APSchuurman <a.p.schuurman@gmail.com>
2016-05-15 20:41:32 +0200

    65 - First project setup (Angular) ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=65 )

--- italent/src/main/webapp/scripts/controllers/homeController.js
+++ italent/src/main/webapp/scripts/controllers/homeController.js
@@ -5,22 +5,24 @@ angular.module('iTalentApp')
     .controller('homeController', ['$scope', '$rootScope', '$location', 'projectService', 'likeService', 'subscriberStudentService', 'subscriberDocentService', 'categoryService', function ($scope, $rootScope, $location, projectService, likeService, subscriberStudentService, subscriberDocentService, categoryService) {
         categoryService.list().then(function (categories) {
             $scope.categories = categories;
-            
-            $scope.categoryFilterArray = new Array();
+
+            $scope.categoryFilterArray = [];
             for (var i = 0; i < categories.length; i++) {
             	$scope.categoryFilterArray.push({id: categories[i].id, description: categories[i].description, on: true});
 			}
-            
         }, function (err) {
-            console.log('Error getting categories: ' + err)
+            console.log('Error getting categories:');
+            console.log(err);
         });
         
         projectService.listHome().then(function (projects) {
             $scope.projects = projects;
         }, function (err) {
-            console.log('Error getting projects: ' + err)
+            console.log('Error getting projects:');
+            console.log(err);
+
         });
-        
+
         $scope.likeClicked = function (project) {
             if (!project.numberOfLikes) {
                 project.numberOfLikes = 0;
@@ -31,7 +33,12 @@ angular.module('iTalentApp')
                 project.numberOfLikes++;
             }
             project.liked = !project.liked;
-            likeService.saveOrUpdate(project.id);
+            likeService.saveOrUpdate(project.id).then(function () {
+                $scope.message = "Likes updated";
+            }, function (err) {
+                console.log('Error updating likes: ');
+                console.log(err);
+            })
         };
         
         $scope.showDetails = function(id) {
@@ -42,7 +49,8 @@ angular.module('iTalentApp')
             subscriberStudentService.save(id, hours).then(function() {
                 $location.path('/home');
             }, function(err) {
-                console.log('Error saving subscriberStudent.')
+                console.log('Error saving subscriberStudent.');
+                console.log(err);
             })
         };
         
@@ -50,14 +58,15 @@ angular.module('iTalentApp')
             subscriberDocentService.save(id, percentage).then(function() {
                 $location.path('/home');
             }, function(err) {
-                console.log('Error saving subscriberDocent.')
+                console.log('Error saving subscriberDocent.');
+                console.log(err);
             })
         };
 
         $scope.categoryFilter = function(proj){
-    	   for(cat in $scope.categoryFilterArray){
+    	   for(var cat in $scope.categoryFilterArray){
                var t = $scope.categoryFilterArray[cat];
-               if(t.on && proj.category.id == t.id){
+               if(t.on && proj.category.id === t.id){
                    return true;   
                }               
            }
