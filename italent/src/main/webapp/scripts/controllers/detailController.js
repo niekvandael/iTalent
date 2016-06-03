e3f2d8ec775c804824d2575750cd7d2a935f31cb
JesseVranken <jesse.vranken@student.pxl.be>
2016-06-04 00:51:59 +0200

    187 - Popup message on back/subcsribe ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=187 )

--- italent/src/main/webapp/scripts/controllers/detailController.js
+++ italent/src/main/webapp/scripts/controllers/detailController.js
@@ -1,6 +1,6 @@
 angular.module('iTalentApp')
-    .controller('detailController', ['$scope', '$routeParams', '$location', 'projectService', 'likeService', 'subscriberStudentService', 'subscriberDocentService', 'commentService', 
-        function ($scope, $routeParams, $location, projectService, likeService, subscriberStudentService, subscriberDocentService, commentService) {
+    .controller('detailController', ['$scope', '$routeParams', '$location', 'projectService', 'likeService', 'subscriberStudentService', 'subscriberDocentService', 'commentService', 'toastr', 
+        function ($scope, $routeParams, $location, projectService, likeService, subscriberStudentService, subscriberDocentService, commentService, toastr) {
 
         var projectId = $routeParams.id;
         $scope.project = {};
@@ -40,9 +40,11 @@ angular.module('iTalentApp')
 
         $scope.saveSubscriberStudent = function(id, hours) {
             subscriberStudentService.save(id, hours).then(function() {
+            	toastr.success('Je bent ingeschreven', 'Succes!');
                 console.log('Success saving subscriberStudent');
                 $scope.project.canSubscribe = false;
             }, function(err) {
+            	toastr.error('Probleem bij het inschrijven, probeer het nogmaals.', 'Fout!');
                 console.log('Error saving subscriberStudent.');
                 console.log(err);
             })
@@ -59,9 +61,11 @@ angular.module('iTalentApp')
         
         $scope.saveSubscriberDocent = function(id, percentage) {
             subscriberDocentService.save(id, percentage).then(function() {
+            	toastr.success('Je steunt het project', 'Succes!');
                 console.log('Success saving subscriberDocent');
                 $scope.project.canBack = false;
             }, function(err) {
+            	toastr.error('Probleem bij het registreren van ondersteuning, probeer het nogmaals.', 'Fout!');
                 console.log('Error saving subscriberDocent.');
                 console.log(err);
             })
