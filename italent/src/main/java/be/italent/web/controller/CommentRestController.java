f48808ca510ae0672edd3d59a4626abc17d37c16
denniegrondelaers <dennie.grondelaers@gmail.com>
2016-06-11 14:50:29 +0200

    198 - Add Javadoc ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id=198 )

--- italent/src/main/java/be/italent/web/controller/CommentRestController.java
+++ italent/src/main/java/be/italent/web/controller/CommentRestController.java
@@ -24,18 +24,36 @@ public class CommentRestController {
 	@Autowired
 	private UserService userService;
 
+	/**
+	 * Save a {@link Comment} for a specific {@link be.italent.model.Project}
+	 *
+	 * @param projectId {@link int} with the {@link be.italent.model.Project}.id you wish to save the comment for
+	 * @param comment {@link Comment} thhe comment you wish to save
+	 * @param principal {@link Principal}
+     */
 	@Secured({"Docent", "Student"})
 	@RequestMapping(value = "/comments/save/{id}", method = RequestMethod.POST, produces="application/json")
 	public void saveComment(@PathVariable("id") final int projectId, @RequestBody String comment, Principal principal){
 		commentService.saveComment(comment, userService.getUserByUsername(principal.getName()), projectId);
 	}
-	
+
+	/**
+	 * Retrieve a list with {@link Comment}s for a specifit {@link be.italent.model.Project}
+	 *
+	 * @param projectId {@link int} The id of the {@link be.italent.model.Project}
+	 * @return a {@link List} with {@link Comment}s
+     */
 	@Secured({"Docent", "Student"})
 	@RequestMapping(value = "/comments/{id}", method = RequestMethod.GET, produces="application/json")
 	public List<Comment> getComments(@PathVariable("id") final int projectId){
 		return commentService.getComments(projectId);
 	}
-	
+
+	/**
+	 * Delete a specific {@link Comment}
+	 *
+	 * @param commentId {@link int} The id of the {@link Comment} to be deleted
+     */
 	@Secured({"Docent"})
 	@RequestMapping(value = "/comments/remove/{id}", method = RequestMethod.DELETE, produces="application/json")
 	public void deleteComment(@PathVariable("id") final int commentId){
