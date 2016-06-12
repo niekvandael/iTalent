2cdff337aea8c7a20409840fcb3090702a7c230d
APSchuurman <a.p.schuurman@gmail.com>
2016-06-12 19:53:38 +0200

    -  ( https://italent.cloudapp.net:9443/ccm/web/projects/iTalent#action=com.ibm.team.workitem.viewWorkItem&id= )

--- italent/src/main/webapp/scripts/controllers/homeController.js
+++ italent/src/main/webapp/scripts/controllers/homeController.js
@@ -1,102 +1,109 @@
 angular.module('iTalentApp')
-    .controller('homeController', ['$scope', '$rootScope', '$location', 'projectService', 'likeService', 'categoryService', 
+    .controller('homeController', ['$scope', '$rootScope', '$location', 'projectService', 'likeService', 'categoryService',
         function ($scope, $rootScope, $location, projectService, likeService, categoryService) {
-       
-    	$scope.tags = [];
-    	$scope.allTags = [];
-    	$scope.shareText = "Check out this great PXL project!";
-    	$scope.location = document.location.origin;
-    	$scope.isArchive = document.location.hash == "#/archive";
-    	$scope.loadingDone = false;
-    	
-    	$scope.loadTags = function(){
-    		return $scope.allTags;
-    	}
-    	categoryService.list().then(function (categories) {
-            $scope.categories = categories;
 
-            $scope.categoryFilterArray = [];
-            $scope.tagsArray = [];
-            for (var i = 0; i < categories.length; i++) {
-            	$scope.categoryFilterArray.push({id: categories[i].categoryId, description: categories[i].description, on: true});
-            	$scope.allTags.push({text: categories[i].description, categoryId: categories[i].categoryId});
-			}
-        }, function (err) {
-            console.log('Error getting categories:');
-            console.log(err);
-        });
-		projectService.listHome($scope.isArchive).then(function (projects) {
-            $scope.projects = projects;
-            $scope.loadingDone = true;
-        }, function (err) {
-            console.log('Error getting projects:');
-            console.log(err);
+            $scope.tags = [];
+            $scope.allTags = [];
+            $scope.shareText = "Check out this great PXL project!";
+            $scope.location = document.location.origin;
+            $scope.isArchive = document.location.hash == "#/archive";
+            $scope.loadingDone = false;
 
-        });	
-        $scope.likeClicked = function (project) {
-            if (!project.numberOfLikes) {
-                project.numberOfLikes = 0;
-            }
-            if (project.liked) {
-                project.numberOfLikes--;
-            } else {
-                project.numberOfLikes++;
-            }
-            project.liked = !project.liked;
-            
-            likeService.saveOrUpdate(project.projectId).then(function () {
-                $scope.message = "Likes updated";
+            $scope.loadTags = function () {
+                return $scope.allTags;
+            };
+
+            categoryService.list().then(function (categories) {
+                $scope.categories = categories;
+                $scope.categoryFilterArray = [];
+                $scope.tagsArray = [];
+                for (var i = 0; i < categories.length; i++) {
+                    $scope.categoryFilterArray.push({
+                        id: categories[i].categoryId,
+                        description: categories[i].description,
+                        on: true
+                    });
+                    $scope.allTags.push({text: categories[i].description, categoryId: categories[i].categoryId});
+                }
             }, function (err) {
-                console.log('Error updating likes: ');
+                console.log('Error getting categories:');
                 console.log(err);
-            })
-        };
-        
-        $scope.showDetails = function(id) {
-            $location.path('/projects/' + id);
-        };
+            });
 
-        $scope.enrollFilter = function(proj){
-        	if($scope.showProjectsICanEnroll){
-        			return proj.canSubscribe;
-        	}
-        	return true;
-        }
-        
-        $scope.backFilter = function(proj){
-        	if($scope.showProjectsICanBack){
-        		return proj.canBack;
-        	}
-        	return true;
-        }
-        
-        $scope.tagFilter = function(proj){
-        	if($scope.tags.length == 0){
-        		return true;
-        	}
-        	
-    		var minOneCategoryIsTag = false;        	
-        	for (var i = 0; i < $scope.tags.length; i++) {
-        		var tag = $scope.tags[i];
-        		
-        		if(tag.categoryId === undefined){
-        			// non-existing tag: try search in title, description, creator first & lastname
-        			if(proj.description.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
-        					|| proj.title.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
-        					|| proj.user.firstname.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
-        					|| proj.user.lastname.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1){
-        				return true;
-        			}
-        		}
-        		
-        		for (var j = 0; j < proj.categories.length; j++) {
-					var projectTag = proj.categories[j];
-	        		
-	                if(projectTag.categoryId === tag.categoryId){
-	                	minOneCategoryIsTag = true;
-	                }    
-				}
-			}
-    		return minOneCategoryIsTag;
-        }
-    }]);
+            projectService.listHome($scope.isArchive).then(function (projects) {
+                $scope.projects = projects;
+                $scope.loadingDone = true;
+            }, function (err) {
+                console.log('Error getting projects:');
+                console.log(err);
+
+            });
+
+            $scope.likeClicked = function (project) {
+                if (!project.numberOfLikes) {
+                    project.numberOfLikes = 0;
+                }
+                if (project.liked) {
+                    project.numberOfLikes--;
+                } else {
+                    project.numberOfLikes++;
+                }
+                project.liked = !project.liked;
+
+                likeService.saveOrUpdate(project.projectId).then(function () {
+                    $scope.message = "Likes updated";
+                }, function (err) {
+                    console.log('Error updating likes: ');
+                    console.log(err);
+                })
+            };
+
+            $scope.showDetails = function (id) {
+                $location.path('/projects/' + id);
+            };
+
+            $scope.enrollFilter = function (proj) {
+                if ($scope.showProjectsICanEnroll) {
+                    return proj.canSubscribe;
+                }
+                return true;
+            };
+
+            $scope.backFilter = function (proj) {
+                if ($scope.showProjectsICanBack) {
+                    return proj.canBack;
+                }
+                return true;
+            };
+
+            $scope.tagFilter = function (proj) {
+                if ($scope.tags.length == 0) {
+                    return true;
+                }
+
+                var minOneCategoryIsTag = false;
+                for (var i = 0; i < $scope.tags.length; i++) {
+                    var tag = $scope.tags[i];
+
+                    if (tag.categoryId === undefined) {
+                        // non-existing tag: try search in title, description, creator first & lastname
+                        if (proj.description.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
+                            || proj.title.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
+                            || proj.user.firstname.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1
+                            || proj.user.lastname.toLowerCase().indexOf($scope.tags[i].text.toLowerCase()) != -1) {
+                            return true;
+                        }
+                    }
+
+                    for (var j = 0; j < proj.categories.length; j++) {
+                        var projectTag = proj.categories[j];
+
+                        if (projectTag.categoryId === tag.categoryId) {
+                            minOneCategoryIsTag = true;
+                        }
+                    }
+                }
+                return minOneCategoryIsTag;
+            }
+        }]
+    );
